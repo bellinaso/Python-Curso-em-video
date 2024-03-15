f159761e4b90b4206ff8765d6b0574ec38a1247a
Bruno Bellinaso Brasil <92125758+bellinaso@users.noreply.github.com>
2024-03-15 18:00:34 -0300

    Update and rename desafio 1 - bem vindo.py to desafio 01 - bem vindo.py

---
+++ Mundo 1/Exercícios/desafio 01 - bem vindo.py
@@ -0,0 +1,3 @@
+nome=input('Qual seu nome? ')
+idade=input('Qual sua idade? ')
+print('Olá {}, muito bom saber que tens {} seja bem vindo!'.format(nome, idade))
--- Mundo 1/Exercícios/desafio 1 - bem vindo.py
+++
@@ -1,3 +0,0 @@
-nome=input('Qual seu nome? ')
-idade=input('Qual sua idade? ')
-print('Olá {}, muito bom saber que tens {} seja bem vindo!'.format(nome, idade))
